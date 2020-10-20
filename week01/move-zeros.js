91260c7a583bccbc09e578ff0c790f1f2df587d1
ZJU <juzhongyu@gmail.com>
2020-10-20 23:44:13 +1100

    movezeroes

---
+++ week01/move-zeros.js
@@ -0,0 +1,10 @@
+var moveZeroes = function (nums) {
+  let idx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[idx] = nums[i];
+      nums[i] = idx === i ? nums[i] : 0;
+      idx++;
+    }
+  }
+};
